1cf5790f943d917aa6857f29a86d4474e49885b0
EWZLMorningstar <ethan.leung@morningstar.com>
2026-08-13 10:16:41 +0100

    Set base path for GitHub Pages deployment

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// If you plan to deploy to GitHub Pages under a repo path, set base to "/REPO_NAME/".
-// Example: base: "/world-timezones/"
 export default defineConfig({
   plugins: [react()],
-  // base: '/',
+  base: '/WorldClock/',
 });
